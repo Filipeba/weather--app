6a89084a1593c16e17b63693dfdcd59296e34cf2
Filipe Bergami Alves <filipeba.ict@gmail.com>
2020-06-13 21:00:00 +0100

    Updates

---
+++ precache-manifest.0b949e9c9439179edf5734196cfb1f76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7093ff762c58312d81a8653303fe1176",
+    "url": "/weather--app/index.html"
+  },
+  {
+    "revision": "af635ee6c96ea0b8db08",
+    "url": "/weather--app/static/css/2.90f23a8e.chunk.css"
+  },
+  {
+    "revision": "c977820c1ed705497561",
+    "url": "/weather--app/static/css/main.08cbc2b8.chunk.css"
+  },
+  {
+    "revision": "af635ee6c96ea0b8db08",
+    "url": "/weather--app/static/js/2.ccdaf8e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather--app/static/js/2.ccdaf8e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c977820c1ed705497561",
+    "url": "/weather--app/static/js/main.4a799a54.chunk.js"
+  },
+  {
+    "revision": "d47bc205717949b81a0c",
+    "url": "/weather--app/static/js/runtime-main.64f3af01.js"
+  },
+  {
+    "revision": "a2255df6b50f84d7389126b418b5b913",
+    "url": "/weather--app/static/media/brand.a2255df6.svg"
+  },
+  {
+    "revision": "70852fc806fcc8c6b78af59dfec1d312",
+    "url": "/weather--app/static/media/mail.70852fc8.svg"
+  }
+]);
